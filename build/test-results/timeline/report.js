d99aca67bb7cfbeb495ffe90ad5000e4dcc6d725
david_alejo <david.cruz@epn.edu.ec>
2023-12-24 19:35:46 -0500

    updated project

--- build/test-results/timeline/report.js
+++ build/test-results/timeline/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"3ed5fd91-1513-4316-b2dd-107640162e4e","feature":"Comprar productos en Opercart","scenario":"Probar movimiento","start":1703460268934,"group":30,"content":"","tags":"@basictest,","end":1703460283330,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"dabeea4e-5a65-43ed-84bb-256de9ae3567","feature":"Comprar productos en Opercart","scenario":"Probar movimiento","start":1703464420668,"group":30,"content":"","tags":"@basictest,","end":1703464434636,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":30,"content":"Thread[#30,ForkJoinPool-2-worker-1,5,main]"}]);
 });
